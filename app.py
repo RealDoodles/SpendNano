5aa1ae9f7b11b4b73db3d1e635dce758bbc7b567
RealDoodles <punk@owns-a-porn.site>
2023-01-16 14:33:27 -0500

    🔨 | Fix add link form functionality

--- app.py
+++ app.py
@@ -20,8 +20,22 @@ def contactpage():
     return render_template("contact.html")
 
 @app.route('/addlink')
-def contactpage():
+def addlinkpage():
     return render_template("addlink.html")
+
+@app.route("/add-link", methods=["POST"])
+def add_link():
+    data = request.get_json()
+    title = data["title"]
+    description = data["description"]
+    link = data["link"]
+    with open("data.json", "r") as json_file:
+        json_data = json.load(json_file)
+    new_link = {"title": title, "description": description, "link": link}
+    json_data.append(new_link)
+    with open("data.json", "w") as json_file:
+        json.dump(json_data, json_file)
+    return "Link added successfully!"
 
 @app.route('/about')
 def about():
